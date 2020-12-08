dc9f7af7289aba195fbedbf1b0e03eb95a767a9c
Fehek <fehek@outlook.com>
2020-12-08 22:01:49 +0800

    r

--- miao/lodash/wenling1-lodash.js
+++ miao/lodash/wenling1-lodash.js
@@ -21,4 +21,90 @@ var wenling1 = function () {
     return result
   }
 
-}
+  function concat(ary, ...val) {
+    let res = [...ary]
+    for (let i = 0; i < val.length; i++) {
+      if (Array.isArray(val[i])) {
+        res.push(...val[i])
+      } else {
+        res.push(val[i])
+      }
+    }
+    return res
+  }
+
+  // join, last, lastIndexOf, fromPairs, head, indexOf, initial, reverse, sortedIndex, every, filter, find, toArray, max, maxBy, min, minBy, sum, sumBy
+
+  function drop(ary, n = 1) {
+    let res = []
+    for (let i = n; i < ary.length; i++) {
+      res.push(ary[i])
+    }
+    return res
+  }
+
+  function dropRight(ary, n = 1) {
+    let res = []
+    for (let i = 0; i < ary.length - n; i++) {
+      res.push(ary[i])
+    }
+    return res
+  }
+
+  function fill(array, value, start = 0, end = array.length) {
+    for (let i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  }
+
+  function identity(value) {
+    return value
+  }
+
+  function findIndex(array, predicate, fromIndex = 0) {
+    for (let i = fromIndex; i < array.length; i++) {
+      if (predicate(array[i], i, array)) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function findLastIndex(array, predicate, fromIndex = array.length - 1) {
+    for (let i = fromIndex; i >= 0; i--) {
+      if (predicate(array[i], i, array)) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function flatten(ary) {
+    return [].concat.apply([], ary)
+  }
+
+  function flattenDeep(ary) {
+    while (ary.some(item => Array.isArray(item))) {
+      ary = [].concat(...ary)
+    }
+    return ary
+  }
+
+  function flattenDepth() {
+
+  }
+
+  return {
+    chunk,
+    compact,
+    concat,
+    drop,
+    dropRight,
+    fill,
+    findIndex,
+    findLastIndex,
+    flatten,
+    flattenDeep,
+  }
+}()
